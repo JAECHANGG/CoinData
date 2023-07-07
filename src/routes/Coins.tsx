09baa9b846875b395e429225a0a78e2d665eec86
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-07-07 16:33:59 +0900

    fix : 홈페이지 타이틀 변경

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
 import { fetchCoins } from "../api";
 import { Helmet } from "react-helmet";
@@ -28,10 +27,10 @@ function Coins() {
   return (
     <Container>
       <Helmet>
-        <title>코인</title>
+        <title>virtual asset</title>
       </Helmet>
       <Header>
-        <Title>코인</Title>
+        <Title>Virtual Asset Top 100</Title>
       </Header>
       {isLoading ? <Loader>Loading...</Loader> : <CoinTable data={data} />}
     </Container>
@@ -53,27 +52,6 @@ const Header = styled.header`
   align-items: center;
 `;
 
-const Coin = styled.li`
-  background-color: ${(props) => props.theme.cardBgColor};
-  color: ${(props) => props.theme.textColor};
-  border-radius: 15px;
-  margin-bottom: 10px;
-  border: 1px solid white;
-  // 패딩 값에 따라 a의 범위가 달라진다.
-  a {
-    padding: 20px;
-    transition: color 0.2s ease-in;
-    display: flex;
-    align-items: center;
-  }
-  // Coin에 마우스를 올렸을 때 a태그를 타겟으로 함. => 여기에서 a태그는 Link태그
-  &:hover {
-    a {
-      color: ${(props) => props.theme.accentColor};
-    }
-  }
-`;
-
 const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
@@ -83,21 +61,3 @@ const Loader = styled.span`
   text-align: center;
   display: block;
 `;
-
-// <CoinsList>
-// {data?.slice(0, 100).map((coin) => (
-//   <Coin key={coin.id}>
-//     {/* 링크를 통해 데이터를 다른 화면으로 보낼 수 있는 방법 (비하인드 더 씬 or state) */}
-//     {/* 여기에서는 Coin.tsx로 이 파일에서 받아온 API 데이터들을 보내주는 것 */}
-//     {/* 굳이 두 번 받아올 필요 없이 성능면에서나 코드면에서 더 좋음 */}
-//     {/* 홈 화면을 거치지 않고 바로 Coin 상세페이지로 들어가면 클릭해서 state를 보내주지 않았기 때문에 즉, API를 받아오지 않았기 때문에 에러가 뜬다. */}
-//     <Link to={`/${coin.id}`} state={{ name: coin.name }}>
-//       {/* 각각의 코인이 갖고 있는 심볼 값이 주소의 마지막이랑 같기 때문에 동적으로 처리해줌 */}
-//       <Img
-//         src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
-//       />
-//       {coin.name} &rarr;
-//     </Link>
-//   </Coin>
-// ))}
-// </CoinsList>
